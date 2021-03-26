de8a46fc1dce7f7de4309ee6be498d76ad72d9a0
hexpwn <hexpwn>
2021-03-26 01:35:37 +0000

    Adding some console colors
    Who doesn't like excessive colors in their CLI tools? (remember to add a
    	flag to disable this for the purists)

--- readuf2.py
+++ readuf2.py
@@ -13,10 +13,19 @@
 from uf2 import UF2
 import argparse
 
+# Console shenanigans
+G = '\033[32m'
+W = '\033[0m'
+R = '\033[31m'
+
 # Parse the arguments
 parser = argparse.ArgumentParser(description='Get an overview of UF2 files. Like `readelf`, but for UF2 files.')
 parser.add_argument('filename', metavar='FILENAME', type=str, nargs=1, help='the path to a UF2 file to be analyzed')
 
 args = parser.parse_args()
 
+# Initialize the UF2 object
+obj = UF2(args.filename[0])
+
+print(f'{G}[:)]{W} Parsing {obj.filename}')
 
