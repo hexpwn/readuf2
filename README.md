d5adf5fb314bbcec85963643fa2542e4417a4e77
hexpwn <hexpwn>
2021-03-26 01:19:21 +0000

    first_commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# readuf2
+`readuf2` is supposed to be a `readelf` for [UF2](https://github.com/Microsoft/uf2) files.
+
+A simple tool to get a quick understanding of what a UF2 file is packing.
+
+## Requirements
+* Python 3
+
+## Usage
+`python3 readbuf2 FILENAME`
---
+++ readuf2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+'''
+    https://github.com/hexpwn/readuf2
+    Version: 0.0a
+
+    `readuf2` is supposed to be a `readelf` for UF2 files. 
+    A simple tool to get a quick understanding of what a UF2 files is packing.
+
+    License: IDGAF 1.0
+'''
+
+import argparse
+
+# Parse the arguments
+parser = argparse.ArgumentParser(description='Get an overview of UF2 files. Like `readelf`, but for UF2 files.')
+parser.add_argument('filename', metavar='FILENAME', type=str, nargs=1, help='the path to a UF2 file to be analyzed')
+
+args = parser.parse_args()
+
+
