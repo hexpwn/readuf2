17726a99cdb14dbd0985c0b23747024d8b1666d4
hexpwn <hexpwn>
2021-03-26 01:47:54 +0000

    Adding important variables used for parsing

--- readuf2.py
+++ readuf2.py
@@ -18,14 +18,46 @@ G = '\033[32m'
 W = '\033[0m'
 R = '\033[31m'
 
+# Important variables
+magic_number        = '\x0a\x32\x46\x55'
+second_magic_number = '\x9e\x5d\x51\x57'
+final_magic_number  = '\x0a\xb1\x6f\x30'
+
+families = {
+    'SAMD21': 0x68ed2b88,
+    'SAML21': 0x1851780a,
+    'SAMD51': 0x55114460,
+    'NRF52': 0x1b57745f,
+    'STM32F0': 0x647824b6,
+    'STM32F1': 0x5ee21072,
+    'STM32F2': 0x5d1a0a2e,
+    'STM32F3': 0x6b846188,
+    'STM32F4': 0x57755a57,
+    'STM32F7': 0x53b80f00,
+    'STM32G0': 0x300f5633,
+    'STM32G4': 0x4c71240a,
+    'STM32H7': 0x6db66082,
+    'STM32L0': 0x202e3a91,
+    'STM32L1': 0x1e1f432d,
+    'STM32L4': 0x00ff6919,
+    'STM32L5': 0x04240bdf,
+    'STM32WB': 0x70d16653,
+    'STM32WL': 0x21460ff0,
+    'ATMEGA32': 0x16573617,
+    'MIMXRT10XX': 0x4FB2D5BD,
+    'LPC55': 0x2abc77ec,
+    'GD32F350': 0x31D228C6,
+    'ESP32S2': 0xbfdd4eee,
+    'RP2040': 0xe48bff56
+}
+
 # Parse the arguments
 parser = argparse.ArgumentParser(description='Get an overview of UF2 files. Like `readelf`, but for UF2 files.')
 parser.add_argument('filename', metavar='FILENAME', type=str, nargs=1, help='the path to a UF2 file to be analyzed')
-
 args = parser.parse_args()
 
 # Initialize the UF2 object
 obj = UF2(args.filename[0])
-
 print(f'{G}[:)]{W} Parsing {obj.filename}')
+
 
